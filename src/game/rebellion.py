8e1ba2a00a29ac50f2e9130e0ddff93ff35703c6
Magister Ludi <ludi@monsterworks.com>
2024-02-01 12:56:03 -0800

    Corrected some mistakes in the last commit. All tests now pass. Time to play around again.

--- src/game/rebellion.py
+++ src/game/rebellion.py
@@ -5,17 +5,17 @@ import sympy as sp
 from icecream import ic  # type: ignore
 
 import mwmath.markov as mkv
+from util.unique import unique
 
 ic.disable()
 
 # region Exciting Space Battle
 
-# todo: doc strings
-Y_WING_DAMAGE = 0
-CORVETTE_DAMAGE = 1
-DESTROYER_DAMAGE = 2
+# region Constants
 
-BATTLE_STATES = {
+Damage = tuple[int, int, int]
+
+EXCITING_BATTLE_STATES: dict[int, set[Damage]] = {
     1: {(0, 0, 0)},
     2: {(0, 1, 0)},
     3: {(0, 2, 0)},
@@ -53,16 +53,19 @@ BATTLE_STATES = {
     },
     14: {(1, 2, 4)},  # all destroyed
 }
-assert sum((len(value) for value in BATTLE_STATES.values())) == 2 * 3 * 5
+assert sum((len(value) for value in EXCITING_BATTLE_STATES.values())) == 2 * 3 * 5
+
+StateDistribution = dict[int, sp.Rational]
 
 
-def state_for_damage(damage):
-    for key, value in BATTLE_STATES.items():
+def state_for_damage(damage: Damage) -> int:
+    for key, value in EXCITING_BATTLE_STATES.items():
         if damage in value:
             return key
     assert False, f"could not find state for {damage=}"
 
 
+ZERO = sp.Rational(0, 1)
 ONE_SIXTH = sp.Rational(1, 6)
 ONE_QUARTER = sp.Rational(1, 4)
 ONE_THIRD = sp.Rational(1, 3)
@@ -70,32 +73,48 @@ ONE_HALF = sp.Rational(1, 2)
 FIVE_SIXTH = sp.Rational(5, 6)
 ONE = sp.Rational(1, 1)
 
+DamageDistribution = dict[tuple[int, int, int], sp.Rational]
+
+Y_WING_DAMAGE = 0
+CORVETTE_DAMAGE = 1
+DESTROYER_DAMAGE = 2
+
+Hits = tuple[int, int, int, int]
+
 BLACK_HIT = 0
 RED_HIT = 1
 CRITICAL_HIT = 2
 MISS = 3
 
-Y_WING_HIT_DISTRIBUTION = {  # Y-wing rolls red
+HitDistribution = dict[Hits, sp.Rational]
+
+Y_WING_HIT_DISTRIBUTION: HitDistribution = {
+    # Y-wing rolls red
     (0, 0, 0, 1): ONE_HALF,  # M
-    (0, 1, 0, 0): ONE_QUARTER,  # R
-    (0, 0, 1, 0): ONE_QUARTER  # C
+    (0, 1, 0, 0): ONE_THIRD,  # R
+    (0, 0, 1, 0): ONE_SIXTH,  # C
 }
 assert mkv.is_distribution(Y_WING_HIT_DISTRIBUTION)
 for KEY in Y_WING_HIT_DISTRIBUTION:
     assert sum(KEY) == 1
 
-CORVETTE_HIT_DISTRIBUTION = {  # corvette rolls black, red
-    (0, 0, 0, 2): FIVE_SIXTH * ONE_HALF,  # MM
-    (0, 1, 0, 1): FIVE_SIXTH * ONE_HALF,  # MH
-    (0, 0, 1, 1): ONE_SIXTH * ONE_HALF,  # CM
-    (0, 1, 1, 0): ONE_SIXTH * ONE_THIRD,  # CH
+CORVETTE_HIT_DISTRIBUTION: HitDistribution = {
+    # corvette rolls black, red
+    (0, 0, 0, 2): ONE_HALF * ONE_HALF,  # MM
+    (1, 0, 0, 1): ONE_THIRD * ONE_HALF,  # BM
+    (0, 0, 1, 1): ONE_SIXTH * ONE_HALF + ONE_HALF * ONE_SIXTH,  # CM + MR
+    (0, 1, 0, 1): ONE_HALF * ONE_THIRD,  # MR
+    (1, 1, 0, 0): ONE_THIRD * ONE_THIRD,  # BR
+    (0, 1, 1, 0): ONE_SIXTH * ONE_THIRD,  # CR
+    (1, 0, 1, 0): ONE_THIRD * ONE_SIXTH,  # BC
     (0, 0, 2, 0): ONE_SIXTH * ONE_SIXTH,  # CC
 }
 assert mkv.is_distribution(CORVETTE_HIT_DISTRIBUTION)
 for KEY in CORVETTE_HIT_DISTRIBUTION:
     assert sum(KEY) == 2
 
-EMPIRE_HIT_DISTRIBUTION = {  # destroyer rolls black, red, red
+EMPIRE_HIT_DISTRIBUTION: HitDistribution = {
+    # destroyer rolls black, red, red
     # black, red, critical
     (0, 0, 0, 3): ONE_HALF * ONE_HALF * ONE_HALF,  # MMM
     (0, 0, 1, 2): ONE_SIXTH * ONE_HALF * ONE_HALF
@@ -129,79 +148,124 @@ assert mkv.is_distribution(EMPIRE_HIT_DISTRIBUTION)
 for KEY in EMPIRE_HIT_DISTRIBUTION:
     assert sum(KEY) == 3
 
-def apply_hits_to_destroyer(destroyer_damage, hits):
-    applied_hits = hits[RED_HIT] + hits[CRITICAL_HIT]
-    destroyer_damage_new = min(destroyer_damage + applied_hits, 4)
+def sum_hits(lhs: Hits, rhs: Hits) -> Hits:
+    return lhs[0] + rhs[0], lhs[1] + rhs[1], lhs[2] + rhs[2], lhs[3] + rhs[3]
+
+# endregion
+
+def apply_hits_to_y_wing(y_wing_damage: int, hits: Hits) -> int:
+    applied_hits = min(1 - y_wing_damage, hits[BLACK_HIT] + hits[CRITICAL_HIT])
+    y_wing_damage_new = y_wing_damage + applied_hits
+    assert y_wing_damage_new <= 1
+    return y_wing_damage_new
+
+def apply_hits_to_tie_fighter(tie_fighter_damage: int, hits: Hits) -> int:
+    applied_hits = min(1 - tie_fighter_damage, hits[BLACK_HIT] + hits[CRITICAL_HIT])
+    tie_fighter_damage_new = tie_fighter_damage + applied_hits
+    assert tie_fighter_damage_new <= 1
+    return tie_fighter_damage_new
+
+def apply_hits_to_destroyer(destroyer_damage: int, hits: Hits) -> int:
+    applied_hits = min(4 - destroyer_damage, hits[RED_HIT] + hits[CRITICAL_HIT])
+    destroyer_damage_new = destroyer_damage + applied_hits
+    assert destroyer_damage_new <= 4
     return destroyer_damage_new
 
+def apply_hits_to_rebels(
+    y_wing_damage: int, corvette_damage: int, hits: Hits
+) -> tuple[int, int]:
+    mutable_hits = list(hits)
 
-def apply_hits_to_rebels(y_wing_damage, corvette_damage, hits):
-    hits = list(hits)
     # pile the black hits on the y-wing
-    applied_damage = min(1 - y_wing_damage, hits[BLACK_HIT])
+    applied_damage = min(1 - y_wing_damage, mutable_hits[BLACK_HIT])
     y_wing_damage += applied_damage
-    hits[BLACK_HIT] -= applied_damage
+    mutable_hits[BLACK_HIT] -= applied_damage
 
     # pile the red hits on the corvette
-    applied_damage = min(2 - corvette_damage, hits[RED_HIT])
+    applied_damage = min(2 - corvette_damage, mutable_hits[RED_HIT])
     corvette_damage += applied_damage
-    hits[RED_HIT] -= applied_damage
+    mutable_hits[RED_HIT] -= applied_damage
 
     # decide where to place the critical hits
-    if corvette_damage + hits[CRITICAL_HIT] >= 2:
+    if corvette_damage + mutable_hits[CRITICAL_HIT] >= 2:
         # we can destroy the corvette
-        applied_damage = min(2 - corvette_damage, hits[CRITICAL_HIT])
+        applied_damage = min(2 - corvette_damage, mutable_hits[CRITICAL_HIT])
         corvette_damage += applied_damage
-        hits[CRITICAL_HIT] -= applied_damage
-    if y_wing_damage + hits[CRITICAL_HIT] >= 1:
+        mutable_hits[CRITICAL_HIT] -= applied_damage
+    if y_wing_damage + mutable_hits[CRITICAL_HIT] >= 1:
         # we can destroy the y_wing
-        applied_damage = min(1 - y_wing_damage, hits[CRITICAL_HIT])
+        applied_damage = min(1 - y_wing_damage, mutable_hits[CRITICAL_HIT])
         y_wing_damage += applied_damage
-        hits[CRITICAL_HIT] -= applied_damage
-    if hits[CRITICAL_HIT] > 0:
+        mutable_hits[CRITICAL_HIT] -= applied_damage
+    if mutable_hits[CRITICAL_HIT] > 0:
         # we damage the corvette
-        applied_damage = min(2 - corvette_damage, hits[CRITICAL_HIT])
+        applied_damage = min(2 - corvette_damage, mutable_hits[CRITICAL_HIT])
         corvette_damage += applied_damage
-        hits[CRITICAL_HIT] -= applied_damage
+        mutable_hits[CRITICAL_HIT] -= applied_damage
 
     # sanity checks
-    if hits[BLACK_HIT] > 0:
+    if mutable_hits[BLACK_HIT] > 0:
         # we have left over black damage, so y_wing should be destroyed
         assert y_wing_damage == 1
 
-    if hits[RED_HIT] > 0:
+    if mutable_hits[RED_HIT] > 0:
         # we have left over red damage, so corvette should be destroyed
         assert corvette_damage == 2
 
-    if hits[CRITICAL_HIT] > 0:
+    if mutable_hits[CRITICAL_HIT] > 0:
         # we have left over critical damage, so both rebels should be destroyed
         assert y_wing_damage == 1
         assert corvette_damage == 2
 
+    assert y_wing_damage <= 1
+    assert corvette_damage <= 2
     return y_wing_damage, corvette_damage
 
 
-def merge_rebel_hit_distributions(y_wing_distribution, corvette_distribution):
-    merged_distribution = defaultdict(int)
+def merge_rebel_hit_distributions(
+    y_wing_distribution: HitDistribution, corvette_distribution: HitDistribution
+) -> HitDistribution:
+    """
+
+    :param y_wing_distribution:
+    :param corvette_distribution:
+    :return:
+    """
+    assert mkv.is_distribution(y_wing_distribution)
+    assert mkv.is_distribution(corvette_distribution)
+    merged_distribution: HitDistribution = defaultdict(mkv.zero)
     for y_hits, y_prob in y_wing_distribution.items():
         for cor_hits, cor_prob in corvette_distribution.items():
-            total_hits = tuple(sum(value) for value in zip(y_hits, cor_hits))
-            merged_distribution[total_hits] += y_prob * cor_prob
+            total_hits = sum_hits(y_hits, cor_hits)
+            total_prob = y_prob * cor_prob
+            merged_distribution[total_hits] += total_prob
+    ic(merged_distribution)
     assert mkv.is_distribution(merged_distribution)
     return merged_distribution
 
 
-def merge_damage_distributions(rebel_damage_distribution, empire_damage_distribution):
+def merge_damage_distributions(
+    rebel_damage_distribution: dict[tuple[int, int], sp.Rational],
+    empire_damage_distribution: dict[int, sp.Rational],
+) -> StateDistribution:
+    """
+
+    :param rebel_damage_distribution:
+    :param empire_damage_distribution:
+    :return:
+    """
     ic(rebel_damage_distribution)
     ic(empire_damage_distribution)
-    merged_damage_distribution = defaultdict(int)
+    merged_damage_distribution: dict[tuple[int, int, int], sp.Rational] = defaultdict(mkv.zero)
     for rebel_dam, rebel_prob in rebel_damage_distribution.items():
         for emp_dam, emp_prob in empire_damage_distribution.items():
-            merged_damage_distribution[(*rebel_dam, emp_dam)] += rebel_prob * emp_prob
+            merged_damage_distribution[(rebel_dam[0], rebel_dam[1], emp_dam)] += (
+                rebel_prob * emp_prob
+            )
     assert mkv.is_distribution(merged_damage_distribution)
     ic(merged_damage_distribution)
 
-    state_distribution = defaultdict(int)
+    state_distribution: StateDistribution = defaultdict(mkv.zero)
     for damage, prob in merged_damage_distribution.items():
         state_distribution[state_for_damage(damage)] += prob
 
@@ -209,12 +273,11 @@ def merge_damage_distributions(rebel_damage_distribution, empire_damage_distribu
     return state_distribution
 
 
-def exciting_transition_distribution(state):
+def exciting_transition_distribution(state: int) -> StateDistribution:
     if state > 10:
-        return {state: 1}
+        return {state: ONE}
 
-    assert len(BATTLE_STATES[state]) == 1
-    damage = next(iter(BATTLE_STATES[state]))
+    damage = unique(EXCITING_BATTLE_STATES[state])
     y_wing_damage = damage[Y_WING_DAMAGE]
     corvette_damage = damage[CORVETTE_DAMAGE]
     destroyer_damage = damage[DESTROYER_DAMAGE]
@@ -223,22 +286,23 @@ def exciting_transition_distribution(state):
         y_wing_hit_distribution = Y_WING_HIT_DISTRIBUTION
     else:
         y_wing_hit_distribution = {(0, 0, 0, 1): ONE}
+    mkv.is_distribution(y_wing_hit_distribution)
 
     if corvette_damage < 2:
         corvette_hit_distribution = CORVETTE_HIT_DISTRIBUTION
     else:
         corvette_hit_distribution = {(0, 0, 0, 2): ONE}
-    ic(corvette_hit_distribution)
-    ic(y_wing_hit_distribution)
+    mkv.is_distribution(corvette_hit_distribution)
 
     rebel_hit_distribution = merge_rebel_hit_distributions(
         y_wing_hit_distribution, corvette_hit_distribution
     )
     ic(rebel_hit_distribution)
 
-    rebel_damage_distribution = defaultdict(int)
     empire_hit_distribution = EMPIRE_HIT_DISTRIBUTION
     ic(empire_hit_distribution)
+
+    rebel_damage_distribution: dict[tuple[int, int], sp.Rational] = defaultdict(mkv.zero)
     for hits, prob in empire_hit_distribution.items():
         y_wing_damage_new, corv_damage_new = apply_hits_to_rebels(
             y_wing_damage, corvette_damage, hits
@@ -246,7 +310,7 @@ def exciting_transition_distribution(state):
         rebel_damage_distribution[(y_wing_damage_new, corv_damage_new)] += prob
     assert mkv.is_distribution(rebel_damage_distribution)
 
-    empire_damage_distribution = defaultdict(int)
+    empire_damage_distribution: dict[int, sp.Rational] = defaultdict(mkv.zero)
     for hits, prob in rebel_hit_distribution.items():
         destroyer_damage_new = apply_hits_to_destroyer(destroyer_damage, hits)
         empire_damage_distribution[destroyer_damage_new] += prob
@@ -255,8 +319,8 @@ def exciting_transition_distribution(state):
     state_distribution = merge_damage_distributions(
         rebel_damage_distribution, empire_damage_distribution
     )
-
     assert mkv.is_distribution(state_distribution)
+
     return state_distribution
 
 
@@ -265,51 +329,50 @@ def exciting_transition_distribution(state):
 # region Probabalistic Battle
 
 
-def black_die_result():
-    roll = rnd.randint(1, 6)
-    if roll in {1, 2, 3}:
-        return MISS
-    elif roll in {4, 5}:
-        return BLACK_HIT
-    else:
-        assert roll == 6
-        return CRITICAL_HIT
+def black_die_result(dice_count: int = 1) -> Hits:
+    hit_list: list[int] = [0, 0, 0, 0]
+    for _ in range(dice_count):
+        roll = rnd.randint(1, 6)
+        if roll in {1, 2, 3}:
+            hit_list[MISS] += 1
+        elif roll in {4, 5}:
+            hit_list[BLACK_HIT] += 1
+        else:
+            assert roll == 6
+            hit_list[CRITICAL_HIT] += 1
+    hits = (hit_list[0], hit_list[1], hit_list[2], hit_list[3])
+    return hits
 
 
-def red_die_result():
-    roll = rnd.randint(1, 6)
-    if roll in {1, 2, 3}:
-        return MISS
-    elif roll in {4, 5}:
-        return RED_HIT
-    else:
-        assert roll == 6
-        return CRITICAL_HIT
+def red_die_result(dice_count: int = 1) -> Hits:
+    hit_list: list[int] = [0, 0, 0, 0]
+    for _ in range(dice_count):
+        roll = rnd.randint(1, 6)
+        if roll in {1, 2, 3}:
+            hit_list[MISS] += 1
+        elif roll in {4, 5}:
+            hit_list[RED_HIT] += 1
+        else:
+            assert roll == 6
+            hit_list[CRITICAL_HIT] += 1
+    hits = (hit_list[0], hit_list[1], hit_list[2], hit_list[3])
+    return hits
 
 
-def roll_y_wing_attack():
-    result = [0, 0, 0, 0]
-    result[red_die_result()] += 1
-    return result
+def roll_tie_fighter_attack() -> Hits:
+    return black_die_result()
+
+def roll_y_wing_attack() -> Hits:
+    return red_die_result()
+
+def roll_corvette_attack() -> Hits:
+    return sum_hits(black_die_result(), red_die_result())
+
+def roll_destroyer_attack() -> Hits:
+    return sum_hits(black_die_result(), red_die_result(2))
 
 
-def roll_corvette_attack():
-    result = [0, 0, 0, 0]
-    result[black_die_result()] += 1
-    result[red_die_result()] += 1
-    return result
-
-
-def roll_destroyer_attack():
-    result = [0, 0, 0, 0]
-    result[black_die_result()] += 1
-    result[red_die_result()] += 1
-    result[red_die_result()] += 1
-
-    return result
-
-
-def combat_step(damage):
+def combat_step(damage: Damage) -> Damage:
     y_wing_damage = damage[Y_WING_DAMAGE]
     corvette_damage = damage[CORVETTE_DAMAGE]
     destroyer_damage = damage[DESTROYER_DAMAGE]
@@ -324,28 +387,28 @@ def combat_step(damage):
     else:
         corvette_hits = (0, 0, 0, 2)
 
-    total_rebel_damage = tuple(sum(value) for value in zip(y_wing_hits, corvette_hits))
-    destroyer_damage_new = apply_hits_to_destroyer(destroyer_damage, total_rebel_damage)
+    total_rebel_hits = sum_hits(y_wing_hits, corvette_hits)
+    total_empire_hits = roll_destroyer_attack()
 
-    total_empire_damage = roll_destroyer_attack()
+    destroyer_damage_new = apply_hits_to_destroyer(destroyer_damage, total_rebel_hits)
+
     y_wing_damage_new, corvette_damage_new = apply_hits_to_rebels(
-        y_wing_damage, corvette_damage, total_empire_damage
+        y_wing_damage, corvette_damage, total_empire_hits
     )
 
     return y_wing_damage_new, corvette_damage_new, destroyer_damage_new
 
 
-def combat_transition(state):
+def combat_transition(state: int) -> int:
     if state > 10:
         return state
-    assert len(BATTLE_STATES[state]) == 1
-    damage = next(iter(BATTLE_STATES[state]))
+    damage = unique(EXCITING_BATTLE_STATES[state])
     damage_new = combat_step(damage)
     state_new = state_for_damage(damage_new)
     return state_new
 
 
-def run_combat(state):
+def run_combat(state: int) -> tuple[int, int]:
     rounds = 0
     while state <= 10:
         state = combat_transition(state)
