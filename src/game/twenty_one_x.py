a6ba535361716ce8652f3233dc50e9ee3ef01e48
Magister Ludi <ludi@monsterworks.com>
2024-01-31 21:07:39 -0800

    Initial cleanup of code.

--- src/game/twenty_one_x.py
+++ src/game/twenty_one_x.py
@@ -3,19 +3,19 @@ from collections import defaultdict
 
 from icecream import ic  # type: ignore
 
-def two_x(x: int, n: int) -> int:
+def two_x(x: int, _n: int) -> int:
     return 2 * x
 
 
-def two_x_minus_six(x: int, n: int) -> int:
+def two_x_minus_six(x: int, _n: int) -> int:
     return 2 * x - 6
 
 
-def ten_minus_two_x(x: int, n: int) -> int:
+def ten_minus_two_x(x: int, _n: int) -> int:
     return 10 - 2 * x
 
 
-def five_n(x: int, n: int) -> int:
+def five_n(_x: int, n: int) -> int:
     return 5 * n
 
 
@@ -23,7 +23,7 @@ def two_n_minus_two_x(x: int, n: int) -> int:
     return 2 * n - 2 * x
 
 
-def one_minus_x(x: int, n: int) -> int:
+def one_minus_x(x: int, _n: int) -> int:
     return 1 - x
 
 
@@ -31,27 +31,27 @@ def n_minus_two_x(x: int, n: int) -> int:
     return n - 2 * x
 
 
-def three_n(x: int, n: int) -> int:
+def three_n(_x: int, n: int) -> int:
     return 3 * n
 
 
-def two_x_minus_ten(x: int, n: int) -> int:
+def two_x_minus_ten(x: int, _n: int) -> int:
     return 2 * x - 10
 
 
-def two_x_plus_four(x: int, n: int) -> int:
+def two_x_plus_four(x: int, _n: int) -> int:
     return 2 * x + 4
 
 
-def seven_n(x: int, n: int) -> int:
+def seven_n(_x: int, n: int) -> int:
     return 7 * n
 
 
-def four_x(x: int, n: int) -> int:
+def four_x(x: int, _n: int) -> int:
     return 4 * x
 
 
-def ten(x: int, n: int) -> int:
+def ten(_x: int, _n: int) -> int:
     return 10
 
 
@@ -59,15 +59,15 @@ def two_n_x(x: int, n: int) -> int:
     return 2 * n * x
 
 
-def neg_three_n(x: int, n: int) -> int:
+def neg_three_n(_x: int, n: int) -> int:
     return -3 * n
 
 
-def neg_two_x(x: int, n: int) -> int:
+def neg_two_x(x: int, _n: int) -> int:
     return -2 * x
 
 
-def neg_five(x: int, n: int) -> int:
+def neg_five(_x: int, _n: int) -> int:
     return -5
 
 
@@ -79,15 +79,15 @@ def n_x(x: int, n: int) -> int:
     return n * x
 
 
-def six_x(x: int, n: int) -> int:
+def six_x(x: int, _n: int) -> int:
     return 6 * x
 
 
-def one_minus_n(x: int, n: int) -> int:
+def one_minus_n(_x: int, n: int) -> int:
     return 1 - n
 
 
-DECK = [
+LEVEL_ONE_DECK = [
     two_x,
     two_x_minus_six,
     ten_minus_two_x,
@@ -126,16 +126,16 @@ def best_value(hand) -> int:
 
 if __name__ == "__main__":
 
-VALUES: defaultdict[int, int] = defaultdict(int)
-HANDS = 0
-for HAND in combinations(deck, 2):
-    # print(best_value(hand), hand)
-    VALUES[best_value(HAND)] += 1
-    HAND += 1
+    VALUES: defaultdict[int, int] = defaultdict(int)
+    HANDS = 0
+    for HAND in combinations(LEVEL_ONE_DECK, 2):
+        # print(best_value(hand), hand)
+        VALUES[best_value(HAND)] += 1
+        HANDS += 1
 
     # for each hand, we should compute the expected value of the hand,
     # assuming we draw whenever the expected value will increase
 
     ic(VALUES)
     ic(HANDS)
-    ic(len(DECK))
+    ic(len(LEVEL_ONE_DECK))
