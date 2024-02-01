c9c42550c10188941427a6118419b8c0b76a7a95
Magister Ludi <ludi@monsterworks.com>
2024-02-01 12:50:32 -0800

    Still not working for parametrized pytests. Need to learn about fixtures?
    Removed undebug since I don't thinkit will work correctly with nesting.

--- src/util/debug.py
+++ src/util/debug.py
@@ -2,6 +2,8 @@ from icecream import ic  # type: ignore
 
 IC_DEPTH = 0
 
+
+# todo: this won't work with parametrized pytests
 def debug(func):
     """
     toggles ic printing for the wrapped function
@@ -17,7 +19,7 @@ def debug(func):
         ic.enable()
         ic(info)
 
-        res = func(*arg)
+        res = ic(func(*arg))
 
         IC_DEPTH -= 1
         if IC_DEPTH <= 0:
@@ -27,36 +29,6 @@ def debug(func):
         else:
             info = f"ic not disabled, current depth: {IC_DEPTH}"
             ic(info)
-
-        return res
-
-    return wrapper
-
-
-def undebug(func):
-    """
-    toggles ic printing for the wrapped function
-
-    :param func: function to be wrapped
-    :return: the wrapped function
-    """
-
-    def wrapper(*arg):
-        global IC_DEPTH
-        IC_DEPTH -= 1
-        info = f"ic disabled"
-        ic(info)
-        ic.disable()
-
-        res = func(*arg)
-
-        IC_DEPTH += 1
-        if IC_DEPTH > 0:
-            info = f"ic reenabled, current depth: {IC_DEPTH}"
-            ic.enable()
-            ic(info)
-        else:
-            ...
 
         return res
 
