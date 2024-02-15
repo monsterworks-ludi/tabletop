43fdfcf4bdfe99a3c855ef350d6e8e9f629b4e6f
Magister Ludi <ludi@monsterworks.com>
2024-02-15 11:27:07 -0800

    Azul examples now work as expected. Still needs documentation and type information.

--- src/game/azul.py
+++ src/game/azul.py
@@ -1,13 +1,15 @@
 import sympy as sp
 import copy
 import itertools
-from collections import defaultdict
+from typing import Callable
 
 from icecream import ic  # type: ignore
 
 from util.debug import checkup
 
 ic.disable()
+
+# todo: docstrings and type information
 
 class AzulBoard:
 
@@ -184,8 +186,7 @@ class AzulTiles:
     def color_string(index):
         return ["*", "b", "y", "r", "k", "c", "1"][index + 1]
 
-    # factory = [2, 1, 1, 0, 1, 0] = 2 blue, 1 yellow, 1 red, 0 black, 1 cyan
-    # center = [1, 3, 2, 1, 3, 1] = 1 blue, 3 yellow, 2 red, 1 black, 3 cyan, 1 firstplayer
+    # factory = [2, 1, 1, 0, 1, 0] = 2 blue, 1 yellow, 1 red, 0 black, 1 cyan, 0 firstplayer
 
     def __init__(self, factories):
         self.factories = factories
@@ -236,34 +237,28 @@ class AzulTiles:
         return nonempty
 
 
-OUTCOMES_FOR_HIST: dict[tuple[str, str, str], list] = defaultdict(list)
-
-
 class AzulState:
 
-    def __init__(self, player, tiles, boards, strategies, history=None, hist=None):
+    def __init__(self, player, tiles, boards, strategies, history=None):
         self.player = player
         self.tiles = tiles
         self.boards = boards
         self.strategies = strategies
         self.history = history if history else ()
-        self.hist = hist if hist else ()
 
     def check(self):
         pass
 
     def optimal_strategy(
         self,
-    ) -> tuple[tuple[int, ...], tuple[str, ...], tuple[str, ...]]:
-        optimal_result: tuple[tuple[int, ...], tuple[str, ...], tuple[str, ...]] = (
+    ) -> tuple[tuple[float, ...], tuple[str, ...]]:
+        optimal_result: tuple[tuple[float, ...], tuple[str, ...]] = (
             tuple(
-                -100_000 if p == self.player else 100_000
-                for p in range(len(self.boards))
+                -sp.oo if p == self.player else sp.oo for p in range(len(self.boards))
             ),
             tuple(),
-            tuple(),
         )
-        optimal_score = -200_000
+        optimal_score = -sp.oo
         for color, factory, row in itertools.product(range(5), range(6), range(5)):
             count = self.tiles.factories[factory][color]
             if count > 0:
@@ -278,40 +273,94 @@ class AzulState:
                     *self.history,
                     f"{AzulTiles.color_string(color)}: {factory} -> {self.player}.{row}",
                 )
-                new_hist = (*self.hist, AzulTiles.color_string(color))
                 state = AzulState(
                     new_player,
                     new_tiles,
                     new_boards,
                     new_strategies,
                     new_history,
-                    new_hist,
                 )
                 result = state.label
                 scores = result[0]
-                delta_score = min(
-                    scores[self.player] - scores[p] for p in range(len(self.boards))
+                result_score = min(
+                    scores[self.player] - scores[p]
+                    for p in range(len(self.boards))
+                    if not p == self.player
                 )
-                if delta_score > optimal_score:
+                if result_score > optimal_score:
                     optimal_result = result
-                    optimal_score = delta_score
+                    optimal_score = result_score
 
         return optimal_result
 
+    def bayesian_strategy(
+        self, weights: Callable
+    ) -> tuple[tuple[float, ...], tuple[str, ...]]:
+        """
+        NOTE: bayesian is not completely random. Which tiles this strategy takes are random,
+        but once the tiles are determined, they are played optimally.
+
+        :param weights:
+        :return:
+        """
+        weighted_scores = tuple(0.0 for _ in range(len(self.boards)))
+        for color, factory in itertools.product(range(5), range(6)):
+            optimal_scores = tuple(-sp.oo if p == self.player else sp.oo for p in range(len(self.boards)))
+            optimal_score = -sp.oo
+            for row in range(5):
+                count = self.tiles.factories[factory][color]
+                if count > 0:
+                    new_player = (self.player + 1) % len(self.boards)
+                    new_tiles = copy.deepcopy(self.tiles)
+                    new_boards = copy.deepcopy(self.boards)
+                    new_strategies = self.strategies
+                    new_boards[self.player].place_in_row(
+                        color=color, count=count, row=row
+                    )
+                    if new_tiles.take(taken_color=color, factory=factory):
+                        self.boards[self.player].broken_tiles += 1
+                    new_history = (
+                        *self.history,
+                        f"*",
+                    )
+                    state = AzulState(
+                        new_player,
+                        new_tiles,
+                        new_boards,
+                        new_strategies,
+                        new_history,
+                    )
+                    row_result = state.label
+                    row_scores = row_result[0]
+                    # row_history = row_results[1] doesn't contain anything useful (we averaged over all possible moves)
+                    row_score = min(
+                        row_scores[self.player] - row_scores[p]
+                        for p in range(len(self.boards))
+                        if not p == self.player
+                    )
+                    if row_score > optimal_score:
+                        optimal_score = row_score
+                        optimal_scores = row_scores
+
+            if not optimal_score == -sp.oo:
+                # this color is available and I have an optimal scoring value for placing this color
+                weighted_scores = tuple(
+                    weighted_scores[i]
+                    + weights(self.tiles, factory, color) * optimal_scores[i]
+                    for i in range(len(weighted_scores))
+                )
+
+        return weighted_scores, new_history
+
     @property
-    def label(self) -> tuple[tuple[int, ...], tuple[str, ...], tuple[str, ...]]:
-        global OUTCOMES_FOR_HIST
+    def label(self) -> tuple[tuple[float, ...], tuple[str, ...]]:
         if not self.tiles:
             for board in self.boards:
                 board.score_round()
                 board.score_game()
-            OUTCOMES_FOR_HIST[self.hist].append(
-                (tuple(board.score for board in self.boards), self.history)
-            )
             result = (
                 tuple(board.score for board in self.boards),
                 self.history,
-                self.hist,
             )
             return result
 
@@ -319,85 +368,4 @@ class AzulState:
 
 
 if __name__ == "__main__":
-
-    BOARD_A = AzulBoard(
-        0,
-        sp.Matrix(
-            [
-                [0, 1, 1, 0, 1],
-                [0, 1, 0, 1, 1],
-                [1, 0, 0, 0, 1],
-                [0, 1, 0, 1, 0],
-                [0, 0, 0, 0, 1],
-            ]
-        ),
-        [
-            [AzulTiles.EMPTY, 0],
-            [AzulTiles.EMPTY, 0],
-            [AzulTiles.BLUE, 2],
-            [AzulTiles.EMPTY, 0],
-            [AzulTiles.RED, 1],
-        ],
-        39,
-        1,
-    )
-
-    BOARD_B = AzulBoard(
-        1,
-        sp.Matrix(
-            [
-                [0, 1, 0, 0, 0],
-                [1, 1, 1, 0, 0],
-                [1, 0, 1, 1, 1],
-                [0, 1, 0, 0, 0],
-                [1, 1, 1, 0, 0],
-            ]
-        ),
-        [
-            [AzulTiles.BLUE, 1],
-            [AzulTiles.EMPTY, 0],
-            [AzulTiles.EMPTY, 0],
-            [AzulTiles.CYAN, 2],
-            [AzulTiles.BLUE, 4],
-        ],
-        30,
-        0,
-    )
-
-    TILES = AzulTiles(
-        [  # b, y, r, k, c, 1 #
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [3, 0, 3, 0, 1, 0],
-        ]
-    )
-
-    STATE = AzulState(
-        0,
-        TILES,
-        (BOARD_A, BOARD_B),
-        (AzulState.optimal_strategy, AzulState.optimal_strategy),
-    )
-    ic.enable()
-    SCORES, HISTORY, HIST = STATE.label
-    ic(SCORES)
-    ic(HISTORY)
-    ic(HIST)
-    for P, SCORE in enumerate(SCORES):
-        ic(P, SCORE)
-    for OUTCOME, SCORE_PAIRS_FULL in OUTCOMES_FOR_HIST.items():
-        MAX_ZERO = 0
-        MAX_ONE = 0
-        BEST_PLAY = ""
-        for SCORE_PAIR_FULL in SCORE_PAIRS_FULL:
-            MAX_ZERO = max(MAX_ZERO, SCORE_PAIR_FULL[0][0])
-            MAX_ONE = max(MAX_ONE, SCORE_PAIR_FULL[0][1])
-            if MAX_ZERO == SCORE_PAIR_FULL[0][0] and MAX_ONE == SCORE_PAIR_FULL[0][1]:
-                BEST_PLAY = SCORE_PAIR_FULL[1]
-        if OUTCOME in {('b', 'r', 'c'), ('b', 'c', 'r'), ('r', 'b', 'c')}:
-            ic(OUTCOME, MAX_ZERO - MAX_ONE, BEST_PLAY)
-
-    ic.disable()
+    ...
--- test/test_azul.py
+++ test/test_azul.py
@@ -1,16 +1,128 @@
 import sympy as sp
 
-from game.azul import use_rational, use_natural
+import game.azul as az
 
 
-def test_rational_dfs():
-    dfs = use_rational()
-    assert dfs("", 0, ["b", "r", "t"]) == ((+3, -3), ("r", "t", "b"))
+class TestAzul:
 
+    @staticmethod
+    def state(strategies) -> az.AzulState:
+        board_zero = az.AzulBoard(
+            0,
+            sp.Matrix(
+                [
+                    [0, 1, 1, 0, 1],
+                    [0, 1, 0, 1, 1],
+                    [1, 0, 0, 0, 1],
+                    [0, 1, 0, 1, 0],
+                    [0, 0, 0, 0, 1],
+                ]
+            ),
+            [
+                [az.AzulTiles.EMPTY, 0],
+                [az.AzulTiles.EMPTY, 0],
+                [az.AzulTiles.BLUE, 2],
+                [az.AzulTiles.EMPTY, 0],
+                [az.AzulTiles.RED, 1],
+            ],
+            29,
+            1,
+        )
 
-def test_natural_dfs():
-    dfs = use_natural()
-    assert dfs("", 0, ["b", "r", "t"]) == (
-        (sp.Rational(17, 5), -sp.Rational(17, 5)),
-        ("t", "*"),
-    )
+        board_one = az.AzulBoard(
+            1,
+            sp.Matrix(
+                [
+                    [0, 1, 0, 0, 0],
+                    [1, 1, 1, 0, 0],
+                    [1, 0, 1, 1, 1],
+                    [0, 1, 0, 0, 0],
+                    [1, 1, 1, 0, 0],
+                ]
+            ),
+            [
+                [az.AzulTiles.BLUE, 1],
+                [az.AzulTiles.EMPTY, 0],
+                [az.AzulTiles.EMPTY, 0],
+                [az.AzulTiles.CYAN, 2],
+                [az.AzulTiles.BLUE, 4],
+            ],
+            20,
+            0,
+        )
+
+        tiles = az.AzulTiles(
+            [  # b, y, r, k, c, 1 #
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [3, 0, 3, 0, 1, 0],
+            ]
+        )
+
+        state = az.AzulState(
+            0,
+            tiles,
+            (board_zero, board_one),
+            strategies,
+        )
+        return state
+
+    @staticmethod
+    def test_rational() -> None:
+        state = TestAzul.state(
+            (az.AzulState.optimal_strategy, az.AzulState.optimal_strategy)
+        )
+        scores, history = state.label
+        assert scores == (28, 25)
+        assert history == ("r: 5 -> 0.3", "c: 5 -> 1.2", "b: 5 -> 0.0")
+
+    @staticmethod
+    def test_bayesian() -> None:
+
+        def weights(tiles: az.AzulTiles, _: int, color: int) -> float:
+
+            assert all(
+                tiles.factories[factory][color] == 0
+                for factory in range(az.AzulTiles.CENTER_PILE)
+                for color in range(6)
+            ), "Unexpected Tile Decision"
+            assert all(
+                tiles.factories[az.AzulTiles.CENTER_PILE][color] == 0
+                for color in {az.AzulTiles.YELLOW, az.AzulTiles.BLACK}
+            ), "Unexpected Tile Decision"
+            assert color in {az.AzulTiles.BLUE, az.AzulTiles.RED, az.AzulTiles.CYAN}, "Unexpected Tile Decision"
+
+            if tiles.factories[az.AzulTiles.CENTER_PILE][az.AzulTiles.RED] == 0:
+                if color == az.AzulTiles.CYAN:
+                    return 0.8
+                else:
+                    assert color == az.AzulTiles.BLUE, "Unexpected Tile Decision"
+                    return 0.2
+
+            elif tiles.factories[az.AzulTiles.CENTER_PILE][az.AzulTiles.BLUE] == 0:
+                if color == az.AzulTiles.CYAN:
+                    return 0.8
+                else:
+                    assert color == az.AzulTiles.RED, "Unexpected Tile Decision"
+                    return 0.2
+            elif tiles.factories[az.AzulTiles.CENTER_PILE][az.AzulTiles.CYAN] == 0:
+                if color == az.AzulTiles.BLUE:
+                    return 0.9
+                else:
+                    assert color == az.AzulTiles.RED, "Unexpected Tile Decision"
+                    return 0.1
+            else:
+                assert False, "Unexpected Tile Decision"
+
+        state = TestAzul.state(
+            (
+                az.AzulState.optimal_strategy,
+                lambda s: az.AzulState.bayesian_strategy(s, weights),
+            )
+        )
+        scores, history = state.label
+        assert abs((scores[0] - scores[1]) - 3.4) < 10**-14
+        assert history == ('c: 5 -> 0.1', '*')
