b4186e5a0c73d45ad8f9d0f49c1454b110f504ec
Magister Ludi <ludi@monsterworks.com>
2024-02-17 06:49:32 -0800

    Passes tests, working on optimizations (stashing unchanging results).

--- src/game/azul.py
+++ src/game/azul.py
@@ -5,7 +5,7 @@ from typing import Callable
 
 from icecream import ic  # type: ignore
 
-from util.debug import checkup
+from util.debug import checkup, debug
 
 ic.disable()
 
@@ -16,24 +16,28 @@ class AzulBoard:
 
     BROKEN_TILES_POINTS = [0, -1, -2, -4, -6, -8, -11, -14]
 
+    ROW_COUNT = 5
+
     @staticmethod
     def column(*, color, row):
-        return (color + row) % 5
+        return (color + row) % AzulBoard.ROW_COUNT
 
     @staticmethod
     def color(*, row, column):
-        return (column - row) % 5
+        return (column - row) % AzulBoard.ROW_COUNT
 
     def __init__(self, player, board=None, rows=None, score=0, broken_tiles=0):
         self.player = player
-        self.board = board if board else sp.zeros(5, 5)
+        self.board = (
+            board if board else sp.zeros(AzulBoard.ROW_COUNT, AzulBoard.ROW_COUNT)
+        )
         self.rows = rows if rows else [[-1, 0], [-1, 0], [-1, 0], [-1, 0], [-1, 0]]
         self.score = score
         self.broken_tiles = broken_tiles
 
     def check(self):
-        assert self.board.shape == (5, 5)
-        assert len(self.rows) == 5
+        assert self.board.shape == (AzulBoard.ROW_COUNT, AzulBoard.ROW_COUNT)
+        assert len(self.rows) == AzulBoard.ROW_COUNT
         for row, (color, count) in enumerate(self.rows):
             assert count <= row + 1
             if color == AzulTiles.EMPTY:
@@ -46,7 +50,7 @@ class AzulBoard:
     @checkup
     def board_string(self, row):
         string = "["
-        for col in range(5):
+        for col in range(AzulBoard.ROW_COUNT):
             if self.board[row, col] == 1:
                 color_index = AzulBoard.color(row=row, column=col)
                 color_string = AzulTiles.color_string(color_index)
@@ -60,7 +64,7 @@ class AzulBoard:
     def row_string(self, index):
         color_index, count = self.rows[index]
         tiles = count * AzulTiles.color_string(color_index)
-        string = (5 - len(tiles)) * " " + tiles
+        string = (AzulBoard.ROW_COUNT - len(tiles)) * " " + tiles
         return string
 
     @checkup
@@ -68,7 +72,7 @@ class AzulBoard:
         string = (
             f"Player {self.player}, Score {self.score}, Broken {self.broken_tiles}\n"
         )
-        for row in range(5):
+        for row in range(AzulBoard.ROW_COUNT):
             string += self.row_string(row) + " " + self.board_string(row) + "\n"
         return string
 
@@ -111,7 +115,7 @@ class AzulBoard:
                 self.score += 1
             else:
                 break
-        for delta in range(1, 5 - row):
+        for delta in range(1, AzulBoard.ROW_COUNT - row):
             if self.board[row + delta, col] == 1:
                 hor_score = True
                 self.score += 1
@@ -123,7 +127,7 @@ class AzulBoard:
                 self.score += 1
             else:
                 break
-        for delta in range(1, 5 - col):
+        for delta in range(1, AzulBoard.ROW_COUNT - col):
             if self.board[row, col + delta] == 1:
                 ver_score = True
                 self.score += 1
@@ -148,24 +152,29 @@ class AzulBoard:
                 self.rows[row] = [AzulTiles.EMPTY, 0]
                 self.score_tile(row, col)
 
-        if self.broken_tiles <= 7:
+        if self.broken_tiles < len(AzulBoard.BROKEN_TILES_POINTS):
             self.score += AzulBoard.BROKEN_TILES_POINTS[self.broken_tiles]
         else:
-            self.score += AzulBoard.BROKEN_TILES_POINTS[7] + 3 * (self.broken_tiles - 7)
+            self.score += AzulBoard.BROKEN_TILES_POINTS[-1] + 3 * (
+                self.broken_tiles - (len(AzulBoard.BROKEN_TILES_POINTS) - 1)
+            )
         self.broken_tiles = 0
 
         self.score = max(self.score, 0)
 
     @checkup
     def score_game(self):
-        for row in range(5):
-            if all(self.board[row, col] == 1 for col in range(5)):
+        for row in range(AzulBoard.ROW_COUNT):
+            if all(self.board[row, col] == 1 for col in range(AzulBoard.ROW_COUNT)):
                 self.score += 2
-        for col in range(5):
-            if all(self.board[row, col] == 1 for row in range(5)):
+        for col in range(AzulBoard.ROW_COUNT):
+            if all(self.board[row, col] == 1 for row in range(AzulBoard.ROW_COUNT)):
                 self.score += 7
-        for diag in range(5):
-            if all(self.board[row, (row + diag) % 5] == 1 for row in range(5)):
+        for diag in range(AzulBoard.ROW_COUNT):
+            if all(
+                self.board[row, (row + diag) % AzulBoard.ROW_COUNT] == 1
+                for row in range(AzulBoard.ROW_COUNT)
+            ):
                 self.score += 10
 
 
@@ -178,10 +187,12 @@ class AzulTiles:
     RED = 2
     BLACK = 3
     CYAN = 4
-
     FIRST_PLAYER = 5
 
+    COLOR_COUNT = 6
+
     CENTER_PILE = 5
+    FACTORY_COUNT = 6
 
     @staticmethod
     def color_string(index):
@@ -193,29 +204,31 @@ class AzulTiles:
         self.factories = factories
 
     def check(self):
-        assert len(self.factories) == AzulTiles.CENTER_PILE + 1
+        assert len(self.factories) == AzulTiles.FACTORY_COUNT
         assert all(
-            len(self.factories[index]) == 6
-            for index in range(AzulTiles.CENTER_PILE + 1)
+            len(self.factories[index]) == AzulTiles.COLOR_COUNT
+            for index in range(AzulTiles.FACTORY_COUNT)
         )
 
     @checkup
     def factory_string(self, factory_index):
         factory = self.factories[factory_index]
         string = ""
-        for index in range(6):
+        for index in range(AzulTiles.FACTORY_COUNT):
             string += factory[index] * AzulTiles.color_string(index)
         return string
 
     @checkup
     def __repr__(self):
-        strings = [self.factory_string(index) for index in range(6)]
+        strings = [
+            self.factory_string(index) for index in range(AzulTiles.FACTORY_COUNT)
+        ]
         return "{" + " | ".join(strings) + "}"
 
     @checkup
     def take(self, *, factory, taken_color):
         if factory < AzulTiles.CENTER_PILE:
-            for color in range(5):
+            for color in range(AzulTiles.COLOR_COUNT):
                 if color != taken_color:
                     self.factories[AzulTiles.CENTER_PILE][color] += self.factories[
                         factory
@@ -229,16 +242,24 @@ class AzulTiles:
                 return True
 
     @checkup
+    def has_tiles(self, index):
+        return any(
+            self.factories[index][color] > 0 for color in range(AzulTiles.COLOR_COUNT)
+        )
+
+    @checkup
     def __bool__(self):
         nonempty = any(
             self.factories[factory][color] > 0
-            for factory in range(AzulTiles.CENTER_PILE + 1)
-            for color in range(6)
+            for factory in range(AzulTiles.FACTORY_COUNT)
+            for color in range(AzulTiles.COLOR_COUNT)
         )
         return nonempty
 
 
 class AzulState:
+
+    Label = tuple[tuple[float, ...], tuple[str, ...]]
 
     def __init__(self, player, tiles, boards, strategies, history=None):
         self.player = player
@@ -247,28 +268,37 @@ class AzulState:
         self.strategies = strategies
         self.history = history if history else ()
 
-    def check(self):
-        pass
+    def __repr__(self):
+        label = self.label
+        return f"Player: {self.player}, Scores: {label[0]}, Moves: {label[1]}"
 
-    def optimal_strategy(
-        self,
-    ) -> tuple[tuple[float, ...], tuple[str, ...]]:
-        optimal_result: tuple[tuple[float, ...], tuple[str, ...]] = (
-            tuple(
-                -sp.oo if p == self.player else sp.oo for p in range(len(self.boards))
-            ),
-            tuple(),
-        )
-        optimal_score = -sp.oo
-        for color, factory, row in itertools.product(range(5), range(6), range(5)):
-            count = self.tiles.factories[factory][color]
-            if count > 0:
+    @property
+    def label(self) -> Label:
+        if not self.tiles:
+            for board in self.boards:
+                board.score_round()
+                board.score_game()
+            result = (
+                tuple(board.score for board in self.boards),
+                self.history,
+            )
+            return result
+
+        return self.strategies[self.player](self)
+
+    def branch_states_for(self, factory, color):
+        count = self.tiles.factories[factory][color]
+        if count > 0:
+            for row in range(AzulBoard.ROW_COUNT):
                 new_player = (self.player + 1) % len(self.boards)
                 new_tiles = copy.deepcopy(self.tiles)
                 new_boards = copy.deepcopy(self.boards)
                 new_strategies = self.strategies
-                new_boards[self.player].place_in_row(color=color, count=count, row=row)
+                new_boards[self.player].place_in_row(
+                    color=color, count=count, row=row
+                )
                 if new_tiles.take(taken_color=color, factory=factory):
+                    # they took the first player marker
                     self.boards[self.player].broken_tiles += 1
                 new_history = (
                     *self.history,
@@ -281,22 +311,60 @@ class AzulState:
                     new_strategies,
                     new_history,
                 )
-                result = state.label
-                scores = result[0]
-                result_score = min(
-                    scores[self.player] - scores[p]
-                    for p in range(len(self.boards))
-                    if not p == self.player
-                )
-                if result_score > optimal_score:
-                    optimal_result = result
-                    optimal_score = result_score
+                yield state
 
-        return optimal_result
+    def optimal_for(self, factory, color):
+        return max((state for state in self.branch_states_for(factory, color)),key=lambda state: state.label[0][self.player])
 
-    def bayesian_strategy(
-        self, weights: Callable
-    ) -> tuple[tuple[float, ...], tuple[str, ...]]:
+    @property
+    def branch_states(self):
+        for factory in range(AzulTiles.COLOR_COUNT):
+            if self.tiles.has_tiles(factory):
+                for color in range(AzulTiles.COLOR_COUNT):
+                    count = self.tiles.factories[factory][color]
+                    if count > 0:
+                        for row in range(AzulBoard.ROW_COUNT):
+                            new_player = (self.player + 1) % len(self.boards)
+                            new_tiles = copy.deepcopy(self.tiles)
+                            new_boards = copy.deepcopy(self.boards)
+                            new_strategies = self.strategies
+                            new_boards[self.player].place_in_row(
+                                color=color, count=count, row=row
+                            )
+                            if new_tiles.take(taken_color=color, factory=factory):
+                                # they took the first player marker
+                                self.boards[self.player].broken_tiles += 1
+                            new_history = (
+                                *self.history,
+                                f"{AzulTiles.color_string(color)}: {factory} -> {self.player}.{row}",
+                            )
+                            state = AzulState(
+                                new_player,
+                                new_tiles,
+                                new_boards,
+                                new_strategies,
+                                new_history,
+                            )
+                            yield state
+
+    def scores_to_score(self, scores):
+        score = min(
+            scores[self.player] - scores[p]
+            for p in range(len(scores))
+            if not p == self.player
+        )
+        return score
+
+    def rational_strategy(
+        self,
+    ) -> Label:
+        optimal_label = max(
+            (state.label for state in self.branch_states),
+            key=lambda label: self.scores_to_score(label[0]),
+        )
+        return optimal_label
+
+    def bayesian_strategy(self, weights: Callable) -> Label:
         """
         NOTE: bayesian is not completely random. Which tiles this strategy takes are random,
         but once the tiles are determined, they are played optimally.
@@ -359,20 +427,75 @@ class AzulState:
 
         return weighted_scores, new_history
 
-    @property
-    def label(self) -> tuple[tuple[float, ...], tuple[str, ...]]:
-        if not self.tiles:
-            for board in self.boards:
-                board.score_round()
-                board.score_game()
-            result = (
-                tuple(board.score for board in self.boards),
-                self.history,
-            )
-            return result
-
-        return self.strategies[self.player](self)
+    # todo monte_carlo strategy
 
 
 if __name__ == "__main__":
-    ...
+
+    def make_state(strategies) -> AzulState:
+        board_zero = AzulBoard(
+            0,
+            sp.Matrix(
+                [
+                    [0, 1, 1, 0, 1],
+                    [0, 1, 0, 1, 1],
+                    [1, 0, 0, 0, 1],
+                    [0, 1, 0, 1, 0],
+                    [0, 0, 0, 0, 1],
+                ]
+            ),
+            [
+                [AzulTiles.EMPTY, 0],
+                [AzulTiles.EMPTY, 0],
+                [AzulTiles.BLUE, 2],
+                [AzulTiles.EMPTY, 0],
+                [AzulTiles.RED, 1],
+            ],
+            29,
+            1,
+        )
+
+        board_one = AzulBoard(
+            1,
+            sp.Matrix(
+                [
+                    [0, 1, 0, 0, 0],
+                    [1, 1, 1, 0, 0],
+                    [1, 0, 1, 1, 1],
+                    [0, 1, 0, 0, 0],
+                    [1, 1, 1, 0, 0],
+                ]
+            ),
+            [
+                [AzulTiles.BLUE, 1],
+                [AzulTiles.EMPTY, 0],
+                [AzulTiles.EMPTY, 0],
+                [AzulTiles.CYAN, 2],
+                [AzulTiles.BLUE, 4],
+            ],
+            20,
+            0,
+        )
+
+        tiles = AzulTiles(
+            [  # b, y, r, k, c, 1 #
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [3, 0, 3, 0, 1, 0],
+            ]
+        )
+
+        state = AzulState(
+            0,
+            tiles,
+            (board_zero, board_one),
+            strategies,
+        )
+        return state
+
+    STATE = make_state((AzulState.rational_strategy, AzulState.rational_strategy))
+
+    print(STATE.rational_strategy())
